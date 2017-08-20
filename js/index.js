4dd4d6fd93440d827407fb341262b1bdceb972fe
impaulapple <impaulapple@gmail.com>
2017-08-20 16:16:10 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -7,6 +7,11 @@ var _easy = ['一口氣從1數到100。', '學大象鼻子，並模仿叫聲', '
 var _normal = ['學狗叫，並用手咬離你最近的那個人', '跟隔壁桌的隨便一個人介紹你自己', '男生和隔壁桌最矮的一個人要Line，女生和最高的人要Line', '裝殘障走秀一圈', '假裝自己是外星人，然後想辦法統治這桌的人', '說出自己一生中最糗的事情 ', '講一個上班(當兵)最幹的事情', '說一個主管的壞話', '講一個同事的八卦', '講一個上班(當兵)遇過最好笑的事情', '對大家大喊，我是豬', '挑一條走道當作是伸展台，學貓走路', '大喊 燃燒吧，小宇宙～', '選一個同性，公主抱轉3圈', '女生的話選一個男生，一邊捶胸一邊說："你好討厭哦"；男生的話隨便選一個女生，捶自己的胸說:"我好討厭喔"', '單手壁咚左前方的一位同學', '雙手壁咚右前方的一位同學', '講一個大學發生最丟臉的事情', '找一張紙寫下自己的line，然後找一個陌生人送出去', '給旁邊的人彈額頭*3', '講一個在大學最不可告人的秘密', '左邊數過去第5個人指定一項物品，放在受逞者的頭上，要他走一圈', '吻桌子5秒', '用舌頭碰鼻子，碰不到的話請撐10秒', '講一個笑話，至少要一個人笑', '用盡你所有的形容詞去形容同桌的一位同學，要讓一半以上的人猜到，切記！只能用形容詞講話', '嘴巴喝一口湯，然後講一句成語，要讓同桌的人都猜到'];
 var _hard = ['真心話or大冒險，由對面的人下指令', '和左前方的人，同時吃一根巧克力棒(可用其他條狀物代替)', '左邊的人當成鋼管，跳鋼管舞10秒', '找一個陌生人和他說："I am king of the world"', '找一桌陌生人們，大喊："I am king of the world"', '給大家搔癢10秒', '給大家搔癢腳底板10秒', '腳底板寫下"反清復明"，由左邊數過去第3個人寫', '給同桌的人30秒的時間挑配特別的果汁，至少要喝3口', '給同桌的人30秒的時間挑配特別的果汁，然後給右邊的同學喝', '倒立10秒，如果自己不能的話，請左邊跟右邊的同學幫忙抓住雙腳', '用屁股寫己的名字，要在5秒內寫完', '與右前方的一位異性十指相扣，對視10秒', '同桌的人每個人夾一道菜給他，並且要吃光光', '深情的吻牆10秒', '抓著入口的門喊「放我出去！」*5次', '嘴巴吸一張紙，右邊數過去第2個人要把它吸走', '四肢壁咚正前方的一位同學', '找一張紙寫下自己的line，然後找一個陌生人，在他面前吃下去', '給旁邊的人彈額頭*5', '哭笑不得：大笑5秒之後，再大哭5秒 反覆3次', '找一個異性陌生人，說：今生我們錯過了，下半生會再相遇的', '喝一大口湯，然後唱一首歌，要讓同桌一半以上的人猜到', '用臉撞破保鮮膜', '去服务台推銷一盤肉', '給大家問三個問題，一定要據實以告'];
 
+var _T_Status = {
+    "b": true,
+    "g": true
+}
+
 var _L_Status = {
     "sl": true,
     "sm": true
@@ -56,9 +61,15 @@ function setBarInit() {
         if (tClose) {
             topEle.style.top = "-20px";
             tClose = false;
+            setStatus("top");
         } else {
             topEle.style.top = "-220px";
+
+            temp_T_status = getNowStatus("top");
+            // if ((JSON.stringify(temp_T_status) != JSON.stringify(_T_Status)) && _L_Status["sl"]) { //有修改
             setStudent();
+            // }
+
             tClose = true;
         }
     });
@@ -92,7 +103,7 @@ function setBarInit() {
                 }
             }
 
-            setStatus("left");
+            // setStatus("left");
             lClose = true;
         }
     });
@@ -104,22 +115,27 @@ function setBarInit() {
             setStatus("right");
         } else {
             rightEle.style.right = "-225px";
-
             temp_R_status = getNowStatus("right");
 
-            if ((JSON.stringify(temp_R_status) != JSON.stringify(_R_Status)) && _L_Status["sl"]) { //有修改
+            if ((JSON.stringify(temp_R_status) != JSON.stringify(_R_Status)) && _L_Status["sm"]) { //有修改
                 setMission();
             }
-
-
-
+            // setStatus("right");
             rClose = true;
         }
     });
 }
 
 function setStatus(type) {
-    if (type == "left") {
+    if (type == "top") {
+        b = byId("boy");
+        g = byId("girl");
+        _T_Status = {
+            "b": isC(b),
+            "g": isC(g)
+        }
+    }
+    else if (type == "left") {
         sl = byId("slot-list");
         sm = byId("slot-mission");
 
@@ -137,14 +153,20 @@ function setStatus(type) {
             "n": isC(n),
             "h": isC(h)
         }
-
     }
-
 }
 
 
 function getNowStatus(type) {
-    if (type == "left") {
+    if (type == "top") {
+        b = byId("boy");
+        g = byId("girl");
+        return {
+            "b": isC(b),
+            "g": isC(g)
+        }
+    }
+    else if (type == "left") {
         sl = byId("slot-list");
         sm = byId("slot-mission");
 
@@ -182,7 +204,7 @@ function setSlot(eleClassName, btnId) {
         number: 1, //要幾格
         spinner: btnId, //轉盤ID '#playFancy'
         easing: 'easeOutSine', //轉盤風格
-        time: 7000, //時間
+        time: 6200, //時間
         loops: 2, //模擬跑幾圈
         onStart: function (spinner) {
             $('.slot').removeClass('winner');
